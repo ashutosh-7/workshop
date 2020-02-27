ce47674b7e492a9e4a96252db2822bddfb87e1d5
Ashutosh Yadav <accesssdenied777@gmail.com>
2020-02-27 09:31:17 +0530

    Update home3.php

--- home3.php
+++ home3.php
@@ -34,7 +34,6 @@ if(isset($_POST['name'])&&isset($_POST['comment']))
     $query="INSERT INTO `register` (`name`,`comment`) VALUES ('$name','$comment');";
      mysqli_query($conn,$query);
 }
- header('location:home3.php');
  }//end of auth
 else 
 header('location:login_page.php');
